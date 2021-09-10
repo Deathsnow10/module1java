cb2ea06b10fc57849f3e45628382611626b5590e
Nikhil Poojari <poojarinikhil01@gmail.com>
2021-09-11 00:40:26 +0530

    sum program

---
+++ src/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args){
+        System.out.print("The sum of the numbers is : ");
+        int num1=4;
+        int num2=3;
+        int num3=4;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
